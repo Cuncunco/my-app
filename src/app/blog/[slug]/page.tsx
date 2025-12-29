540e01ea44c113aeac1bf5b496837ff9419fb124
Cuncunco <victouros@gmail.com>
2025-12-29 17:51:29 -0300

    commit page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,42 +1,32 @@
 import { PostPage as Post } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
-import type { Metadata } from "next";
 import { notFound } from "next/navigation";
+import type { Metadata } from "next";
 
 export async function generateMetadata(
   { params }: { params: { slug: string } }
 ): Promise<Metadata> {
-  const post = allPosts.find((post) => post.slug === params.slug);
-
+  const post = allPosts.find(p => p.slug === params.slug);
   if (!post) return {};
 
   return {
     title: post.title,
     description: post.description,
-    authors: [{ name: post.author.name }],
-    robots: "index, follow",
-    openGraph: {
-      images: [post.image],
-    },
   };
 }
 
-export default async function Page(
+export default function Page(
   { params }: { params: { slug: string } }
 ) {
-  const post = allPosts.find((post) => post.slug === params.slug);
+  const post = allPosts.find(p => p.slug === params.slug);
 
-  if (!post) {
-    notFound();
-  }
+  if (!post) notFound();
 
   return <Post post={post} />;
 }
 
 export function generateStaticParams() {
-  return allPosts.map((post) => ({
+  return allPosts.map(post => ({
     slug: post.slug,
   }));
 }
-
-export const revalidate = 60;
