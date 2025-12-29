3bb85d850ba75460cda961bc02c5b784647c5784
Cuncunco <victouros@gmail.com>
2025-12-29 17:34:24 -0300

    fix next 15 page params typing

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,36 +3,28 @@ import { allPosts } from "contentlayer/generated";
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 
+export async function generateMetadata(
+  { params }: { params: { slug: string } }
+): Promise<Metadata> {
+  const post = allPosts.find((post) => post.slug === params.slug);
 
-type BlogPostPageProps = {
-  params: Promise<{
-    slug: string;
-  }>;
-};
-
-export async function generateMetadata({ params }: BlogPostPageProps): Promise<Metadata> {
-  const { slug } = await params;
-  const post = allPosts.find((post) => post.slug === slug);
-
-  if (!post) {
-    return {}
-  }
+  if (!post) return {};
 
   return {
     title: post.title,
     description: post.description,
     authors: [{ name: post.author.name }],
-    robots: 'index, follow',
+    robots: "index, follow",
     openGraph: {
-      images: [post.image]
-    }
-  }
+      images: [post.image],
+    },
+  };
 }
 
-export default async function PostPage({ params }: BlogPostPageProps) {
-  const { slug } = await params;
-
-  const post = allPosts.find((post) => post.slug === slug);
+export default async function Page(
+  { params }: { params: { slug: string } }
+) {
+  const post = allPosts.find((post) => post.slug === params.slug);
 
   if (!post) {
     notFound();
@@ -41,16 +33,10 @@ export default async function PostPage({ params }: BlogPostPageProps) {
   return <Post post={post} />;
 }
 
-/**
- * Geração estática dos slugs
- */
 export function generateStaticParams() {
   return allPosts.map((post) => ({
     slug: post.slug,
   }));
 }
 
-/**
- * ISR (opcional)
- */
 export const revalidate = 60;
